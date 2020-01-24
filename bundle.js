d92efaa56388204a5e235549c438218705b08b7a
Fabamise Adeolu <fabamiseadeolu@gmail.com>
2020-01-24 23:19:57 +0100

    updating

--- bundle.js
+++ bundle.js
@@ -17117,10 +17117,10 @@
 },{}],2:[function(require,module,exports){
 var _ = require("lodash");
 
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
 
 function setGradient() {
 	body.style.background = 
