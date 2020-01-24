f3cf1aca823d09cf38567559f911734ca5ca7a1a
Fabamise Adeolu <fabamiseadeolu@gmail.com>
2020-01-24 23:26:16 +0100

    updating

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
+
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
